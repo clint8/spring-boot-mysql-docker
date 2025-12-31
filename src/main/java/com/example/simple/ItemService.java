f42f099900e9c54df4536b6ed7280a1f3f3089a3
Clint Rollins <clintrollins@Mac.lan>
2025-12-31 14:17:14 -0700

    Initial commit for spring boot project

---
+++ src/main/java/com/example/simple/ItemService.java
@@ -0,0 +1,25 @@
+package com.example.simple;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item saveItem(Item item) {
+        return itemRepository.save(item);
+    }
+}
